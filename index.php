bd7ce545130c3d91907543454f3c556b801cdcd3
RAX-23 <85556114+RAX-23@users.noreply.github.com>
2021-09-08 17:05:50 +0700

    @initialization

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rax</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <section>
+            <audio src="assets/romantic.mp3" type="adio/mpeg" controls autoplay loop hidden></audio>
+
+            <h1>
+                <span class="glow">R</span>
+                <span class="glow">A</span>
+                <span class="glow">X</span>
+                <span class="glow">&nbsp;</span>
+                <span class="glow">2</span>
+                <span class="glow">3</span>
+
+            </h1>
+
+            <ul>
+                <a href="#">
+                    <li><img src="assets/image/sites/stars/shiningstar2.png" alt="Biodata"><span>Biodata</span></li>
+                </a>
+                <a href="#">
+                    <li><img src="assets/image/sites/stars/shiningstar2.png" alt="Education"><span>Education</span></li>
+                </a>
+                <a href="#">
+                    <li><img src="assets/image/sites/stars/shiningstar2.png" alt="Experiences"><span>Experiences</span></li>
+                </a>
+                <a href="#">
+                    <li><img src="assets/image/sites/stars/shiningstar2.png" alt="Skills"><span>&nbsp;&nbsp;&nbsp;Skills</span></li>
+                </a>
+            </ul>
+
+            <div class="star star1"></div>
+            <div class="star star2"></div>
+            <div class="star star3"></div>
+            <div class="star star4"></div>
+            <div class="star star5"></div>
+            <div class="star star6"></div>
+            <div class="star star7"></div>
+            <div class="star star8"></div>
+
+            <div class="sky-container">
+                <div class="sstar"></div>
+                <div class="sstar"></div>
+                <div class="sstar"></div>
+                <div class="sstar"></div>
+                <div class="sstar"></div>
+            </div>
+
+            <div class="set">
+                <div><img src="assets/image/sites/background/leaves1.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves2.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves3.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves4.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves5.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves1.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves2.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves3.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves4.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves5.png" width="100" alt=""></div>
+            </div>
+            <div class="set set2">
+                <div><img src="assets/image/sites/background/leaves1.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves2.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves3.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves4.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves5.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves1.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves2.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves3.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves4.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves5.png" width="100" alt=""></div>
+            </div>
+            <div class="set set3">
+                <div><img src="assets/image/sites/background/leaves1.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves2.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves3.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves4.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves5.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves1.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves2.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves3.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves4.png" width="100" alt=""></div>
+                <div><img src="assets/image/sites/background/leaves5.png" width="100" alt=""></div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
